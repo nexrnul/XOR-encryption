a891a3f9e6e3a72cf55592b15e2b731aba096511
nexer nul <redpenguin4luca26@gmail.com>
2024-02-27 21:27:49 +0000

    framework

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -35,4 +35,23 @@ def XORonByte(byte, key):
 
     return emsg
 
-print(XORonByte("0010", "0011"))
+def XORonLetter(letter, keyLetter):
+
+    letterBin = encode(letter)
+    keyLetterBin = encode(keyLetter)
+    encryptedLetter = XORonByte(letterBin, keyLetterBin)
+    return decode(encryptedLetter)
+#print(XORonLetter("d", "r"))
+
+
+def XORonSentence(sentence, key):
+    
+    encryptedSentence = ""
+
+    for i in range (len(sentence)):
+        
+        encryptedSentence += XORonLetter(sentence[i], key[i])
+    
+    return encryptedSentence
+
+print(XORonSentence("hello", "world"))
